6c3dc4f24fe8c709bd2b2f7699509683b3ed2e50
mamax51 <max51jeux@gmail.com>
2021-01-26 15:40:04 +0100

    Modif

--- test.py
+++ test.py
@@ -9,28 +9,34 @@ def read_json(file):
         data = json.load(f)
     return data
 
+info = read_json(arg)
+subprocess.run(["git", "clone",info['url']])
 
-
-test = read_json(arg)
-print(test['url'])
-subprocess.run(["git", "clone",test['url']])
-
-id = test['id']
-print(id)
-print(os.getcwd())
+id = info['id']
 path  = os.getcwd()+"/"+id+"/"
-print(path)
 list = os.listdir(path)
-print(list)
+
 i = 0
 while list[i] != test['file']:
 	i+=1
-print(list)
+
 fichier = list[i]
 path2 = path+fichier
 
 output = pypandoc.convert_file(path2, 'docx', outputfile="somefile.docx")
-print(output)
-test['etat'] = 'done'
-print(test)
+
+#Ajout du résultat pour l'envoyer au dépot distant
+subprocess.Popen(['git', 'add', '.'], cwd=path)
+
+#Commit du résultat à envoyer
+subprocess.Popen(['git', 'commit' ,'-m', 'Push du résultat après traitement'], cwd=path)
+
+#Push du projet, rentrée auto du username et du password
+child = pexpect.spawn('git push', cwd=nameRepo)
+child.expect([pexpect.TIMEOUT, "Username for 'https://github.com':"])
+child.sendline("Projet704\n".encode())
+child.expect([pexpect.TIMEOUT, "Password for 'https://Projet704@github.com':"])
+child.sendline("MaxenceThomas51\n".encode())
+child.read()
+
 
